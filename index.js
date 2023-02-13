e4d4387a521dda3f0d77865ed3a945839031b219
Carlos Bermejo Pérez <carlosbermejop@gmail.com>
2023-02-13 10:59:59 +0100

    feat(CI): added CI implementation

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ console.log("commit 2, read functionality");
 console.log("commit 3, create functionality");
 console.log("commit 4, update functionality");
 console.log("commit 5, delete functionality");
+console.log("commit 6, broken CI implementation");
