a0d3fa83bb5644c16f2efa16fc6afd072cae76a2
Carlos Bermejo Pérez <carlosbermejop@gmail.com>
2023-02-13 10:58:39 +0100

    feat(API): delete functionality implemented

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log("initial commit, hello world!");
 console.log("commit 2, read functionality");
 console.log("commit 3, create functionality");
 console.log("commit 4, update functionality");
+console.log("commit 5, delete functionality");
